3d51219bc32239726b3b53df3bf8cfc428c54469
Ayla Crosley <swiedaa1@nku.edu>
2014-01-30 13:35:45 +0000

    Added def create, def show, and def if/else to user)controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,23 @@
 class UserController < ApplicationController
+def show
+ user = User.find(params[:id]
+ @user = UserPresenter.new(user)
+end
+def show
+ @user = current_user.find(params[:id])
+end
+def create
+ post = Post.new(params)
+ post.user = current_user
+ post.save
+	if @post.save
+	 redirect_to posts_path, notice: "Success!"
+	else
+	 render :new
+	end
+end
+
+
+
+
 end
