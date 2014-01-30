f594ea7d62a48f6137852bdba9a17b738fe90765
Ayla Crosley <swiedaa1@nku.edu>
2014-01-30 12:53:42 +0000

    Added def reset_password, def full_name, and def recent_completed to user.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,51 @@
 class PostsController < ApplicationController
-  def new
-  end
+
+def new
+  @post = Post.new
+end
  
 def create
-  @post = Post.new(post_params)
+  @post = Post.new(params[:post].permit(:name,:nickname,:email, :image, :text))
  
-  @post.save
-  redirect_to @post
+  if @post.save
+    redirect_to @post, notice: "Success!" 
+  else
+    render 'new'
+  end
 end
-
 def show
-  @post = Post.find(params[:id])
+  user = User.find(params[:id])
+  @user = UserPresenter.new(user)
 end
 
 def index
   @posts = Post.all
 end
+
+def edit
+  @post = Post.find(params[:id])
+end 
+
+def update
+  @post = Post.find(params[:id])
  
-private
-  def post_params
-    params.require(:post).permit(:title, :text)
+  if @post.update(params[:post].permit(:name,:nickname,:email,:image, :text) )
+    redirect_to @post
+  else
+    render 'edit'
   end
 end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
+private
+  def post_params
+    params.require(:post).permit(:name,:nickname,:email,:image, :text)
+  end
+end
+
