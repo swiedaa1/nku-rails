3cecac72f9ebec6ff82ad2e8133f1a824cb81450
Ayla Crosley <swiedaa1@nku.edu>
2014-01-30 17:59:23 +0000

    removing 'post' and replacing it with 'user' in every file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140129013108) do
+ActiveRecord::Schema.define(version: 20140130174951) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 20140129013108) do
     t.string   "email"
     t.string   "image"
     t.string   "name"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
