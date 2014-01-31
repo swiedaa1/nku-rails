2c7a1afff8f4bc736d0b295d90c3804541f415b8
Ayla Crosley <swiedaa1@nku.edu>
2014-01-31 18:30:40 +0000

    removed _form.html.erb again. Form displays properly under new.html.erb but submit button does not work

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ NkuRails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
+resource :user_path
 resources :user 
 resources :posts do 
 resources :comments
