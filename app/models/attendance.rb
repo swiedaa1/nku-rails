c8e82d09d2afe5873b97b3a904201e3e24b3a337
Ayla Crosley <swiedaa1@nku.edu>
2014-02-21 15:53:59 +0000

    cleaned up users index

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,8 +1,6 @@
 class Attendance < ActiveRecord::Base
   belongs_to :users
-  
-#  validates :user_id, uniqueness: {scope: :attended_on, 
-#    message: "Each student may only attend once per day." }
+  # Make sure :seat is present, is an integer and is only 1-4:
   validates :seat, :inclusion => 1..4
   validates :seat, :numericality => {:only_integer => true}
   validates :seat, presence: true
